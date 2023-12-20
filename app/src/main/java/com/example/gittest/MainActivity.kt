2ecc435faa8066d4f16f3aac81d6ee6f11812b18
Giovanni Javier Lopez Villa <xgiosebsx@gmail.com>
2023-12-20 16:41:47 -0500

    Greeting android was added into MainActivity

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Hello Git")
+                    Greeting("Android")
                 }
             }
         }
